0d5c1c60456b2b427018dbb9d63fca4a94456281
abednego emonena <emonenaabednego@gmail.com>
2025-04-23 03:51:47 +0100

    added an event listener to each button

---
+++ web_programming_1/Travel_blog/script.js
@@ -0,0 +1,9 @@
+let one = document.getElementById("one")
+let two = document.getElementById("two")
+let three = document.getElementById("three")
+
+let content = document.getElementById("content")
+
+one.addEventListener("click", function() {
+    
+})
